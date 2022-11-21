aaf885f67977e9e8ac97045a03a12719529ea325
Gustavo <gustavolerner.hartmann@gmail.com>
2022-11-21 17:42:03 -0300

    feat: balance layout and functions

--- src/components/StatementsContainer.jsx
+++ src/components/StatementsContainer.jsx
@@ -11,9 +11,44 @@ const StatementsContainer = styled.div`
 
   border-radius: 5px;
 
-  display: ${props => props.statementsExist ? "block" : "flex"};
+  display: ${(props) => (props.statementsExist ? "block" : "flex")};
   align-items: center;
   text-align: center;
+
+  position: relative;
+
+  overflow: scroll;
+
+  div:last-child {
+    width: 100%;
+
+    font-size: 17px;
+
+    padding: 5px 10px;
+
+    background-color: white;
+
+    border-top: 1px solid #868686;
+
+    display: flex;
+    justify-content: space-between;
+
+    position: absolute;
+    bottom: 0;
+    z-index: 1;
+
+    p:first-child {
+      font-weight: bold;
+
+      color: black;
+    }
+
+    p:last-child {
+      font-weight: bold;
+
+      color: ${(props) => (props.isNegative ? "#C70000" : "#03AC00")};
+    }
+  }
 `;
 
 export default StatementsContainer;
--- src/pages/Statements/index.jsx
+++ src/pages/Statements/index.jsx
@@ -6,10 +6,12 @@ import { AuthContext } from "../../context/AuthContext";
 import axios from "axios";
 import Statement from "../../components/Statement";
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
 export default function Statements() {
   const [username, setUsername] = useState("");
   const [statementsArray, setStatementsArray] = useState([]);
+  const [balance, setBalance] = useState(0);
   const { token } = useContext(AuthContext);
 
   async function getStatements() {
@@ -20,12 +22,25 @@ export default function Statements() {
       },
     };
 
-    const response = await axios.get(url, config);
+    try {
+      const response = await axios.get(url, config);
 
-    const { user, statements } = response.data;
+      const { user, statements } = response.data;
 
-    setUsername(user.name);
-    setStatementsArray(statements);
+      setUsername(user.name);
+      setStatementsArray(statements);
+      
+      let value = 0;
+      statements.forEach((s) => {
+        value += s.value;
+      });
+      setBalance(value);
+    } catch (err) {
+      Swal.fire({
+        icon: "error",
+        title: err.response.data,
+      });
+    }
   }
 
   useEffect(() => {
@@ -35,10 +50,17 @@ export default function Statements() {
   return (
     <StatementsPageContainer>
       <h1>Olá, {username}</h1>
-      <StatementsContainer statementsExist={statementsArray.length > 0}>
+      <StatementsContainer
+        statementsExist={statementsArray.length > 0}
+        isNegative={String(balance).startsWith("-")}
+      >
         {statementsArray.length > 0
           ? statementsArray.map((s) => <Statement key={s._id} statement={s} />)
           : "Não há registro de entrada ou saída"}
+        <div>
+          <p>SALDO</p>
+          <p>{balance}</p>
+        </div>
       </StatementsContainer>
       <span>
         <Link to={"/nova-entrada"}>
